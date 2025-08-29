864e3719a36c5710ab9dcc50bb48153cf59befcd
Stas <137521612+Stanislav384@users.noreply.github.com>
2025-08-29 20:11:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -29,4 +29,53 @@ q2 = Question('Дім', 'house', 'horse', 'hurry', 'hour')
 q3 = Question('Миша', 'mouse', 'mouth', 'muse', 'museum')
 q4 = Question('Число', 'number', 'digit', 'amount', 'summary')
 
+
+
+radio_buttons = [answer_1,answer_2,answer_3,answer_4]
+questions = [q1, q2, q3, q4]
+
+def new_question():
+    global cur_q
+    cur_q = choice(questions)
+    lb_Question.setText(cur_q.question)
+    Correct.setText(cur_q.answer)
+
+    radio_buttons[0].setText(cur_q.wrong_answer1)
+    radio_buttons[1].setText(cur_q.wrong_answer2)
+    radio_buttons[2].setText(cur_q.wrong_answer3)
+    radio_buttons[3].setText(cur_q.answer)
+
+new_question()
+
+
+def check():
+    RadioGroup.setExclusive(False)
+    for answer in radio_buttons:
+        if answer.isChecked():
+            if answer.text() == Correct.text():
+                cur_q.got_right()
+                Result.setText('Вірно!')
+                answer.setChecked(False)
+                break
+    else:
+        Result.setText('Не вірно!')
+        cur_q.got_wrong()
+
+
+def click_ok():
+    if btn_OK.text() == 'Відповісти':
+        check()
+        RadioGroupBox.hide()
+        AnsGroupBox.show()
+
+        btn_OK.setText('Наступне запитання')
+    else:
+        new_question()
+        RadioGroupBox.show()
+        AnsGroupBox.hide()
+
+        btn_OK.setText('Відповісти')
+
+btn_OK.clicked.connect(click_ok)
+
 app.exec_()
